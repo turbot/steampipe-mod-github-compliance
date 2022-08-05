04eeb179834065006b8ecf4bc306cb56eeddc604
rajmohanty17 <raj@turbot.com>
2022-08-05 13:10:24 +0530

    Updates

--- query/org_manual_control.sql
+++ query/org_manual_control.sql
@@ -1,9 +1,15 @@
+-- select
+--   -- Required Columns
+--   name as resource,
+--   'info' as status,
+--   'Manual verification required with ' as reason,
+--   -- Additional Dimensions
+--   name as organization_name
+-- from
+--   github_my_organization;
+
 select
   -- Required Columns
-  name as resource,
+  'organization' as resource,
   'info' as status,
-  'Manual verification required with ' as reason,
-  -- Additional Dimensions
-  name as organization_name
-from
-  github_my_organization
+  'Manual verification required for organizations ' as reason
