026b03d8888e6786608299719e45864a5265d7cc
Chris Farris <chris@room17.com>
2022-10-28 16:37:23 -0400

    Change status to 'info' when the user is not an owner of the organization to reduce false positives

--- query/organization/org_two_factor_required.sql
+++ query/organization/org_two_factor_required.sql
@@ -2,10 +2,14 @@ select
   -- Required Columns
   html_url as resource,
   case
+    when two_factor_requirement_enabled is null then 'info'
     when two_factor_requirement_enabled then 'ok'
     else 'alarm'
   end as status,
-  coalesce(name, login) || case when (two_factor_requirement_enabled)::bool then ' members require MFA' else ' not enabled with MFA' end || '.' as reason,
+  coalesce(name, login) || case
+    when (two_factor_requirement_enabled)::bool is null then ' is not an organization you are an owner of'
+    when (two_factor_requirement_enabled)::bool then ' members require MFA'
+    else ' not enabled with MFA' end || '.' as reason,
   -- Additional Dimensions
   login
 from
