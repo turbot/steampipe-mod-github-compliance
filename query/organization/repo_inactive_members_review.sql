06c060f972aa2d6890a886e0112fe8b4811f73d0
souravTurbot <78197905+bigdatasourav@users.noreply.github.com>
2022-11-11 01:42:17 +0530

    Update `repo_inactive_members_review` query to only check commits from last 30 days closes #17 (#20)

--- query/organization/repo_inactive_members_review.sql
+++ query/organization/repo_inactive_members_review.sql
@@ -6,13 +6,15 @@ with repo as (
   from
     github_commit c
     join github_my_repository r on c.repository_full_name = r.full_name
+  where
+    author_date >= now() - interval '30 day'
   group by
     repository_full_name,
     author_login
 ),
 logins as (
   select
-    c->> 'login' as login,
+    c ->> 'login' as login,
     full_name as repository_full_name
   from
     github_my_repository,
@@ -21,17 +23,20 @@ logins as (
 select
   -- Required Columns
   l.repository_full_name as resource,
-  case
-    when commit_count is null then 'alarm'
-    else 'ok'
+  case when commit_count is null then
+    'alarm'
+  else
+    'ok'
   end as status,
-  case
-    when commit_count is null then login || ' is an inactive user.'
-    else login || ' is an active user.'
+  case when commit_count is null then
+    login || ' is an inactive user.'
+  else
+    login || ' is an active user.'
   end as reason,
   -- Additional Dimensions
   l.repository_full_name
 from
   logins l
   left join repo r on l.login = r.author_login
-  and l.repository_full_name = r.repository_full_name;
+    and l.repository_full_name = r.repository_full_name;
+
