6110ea1270fad19a75eb7431899c3a445af48d97
Chris Farris <chris@room17.com>
2022-11-15 17:53:32 -0500

    Fix `org_default_repo_permission_none` and `org_default_repo_permission_none_read` queries returning false positives when user is not org owner fixes #22 (#23)

--- query/organization/org_default_repo_permission_none.sql
+++ query/organization/org_default_repo_permission_none.sql
@@ -2,10 +2,16 @@ select
   -- Required Columns
   html_url as resource,
   case
+    when default_repo_permission is null then 'info'
     when default_repo_permission = 'none' then 'ok'
     else 'alarm'
   end as status,
-  coalesce(name, login) || ' base permission is ' || default_repo_permission || '.' as reason,
+  case
+    when default_repo_permission is null then
+      coalesce(name, login) || ' base permission unknown, manual verification required.'
+    else
+      coalesce(name, login) || ' base permission is ' || default_repo_permission || '.'
+  end as reason,
   -- Additional Dimensions
   login
 from
