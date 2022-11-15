de014d1313a79bb77f58108669acae1c32a20844
Chris Farris <chris@room17.com>
2022-11-15 07:51:26 -0500

    Fix - default_repo_permissions when not owner

--- query/organization/org_default_repo_permission_none_read.sql
+++ query/organization/org_default_repo_permission_none_read.sql
@@ -2,10 +2,15 @@ select
   -- Required Columns
   html_url as resource,
   case
+    when default_repo_permission is null then 'skip'
     when default_repo_permission in ('read', 'none') then 'ok'
     else 'alarm'
   end as status,
-  coalesce(name, login) || ' base permission is ' || default_repo_permission || '.' as reason,
+  case
+    when default_repo_permission is null then 'user is not an owner of this repo'
+    else
+      coalesce(name, login) || ' base permission is ' || default_repo_permission || '.'
+  end as reason,
   -- Additional Dimensions
   login
 from
