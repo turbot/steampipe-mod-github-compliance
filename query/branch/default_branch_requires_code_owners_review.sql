4c28dcbbdd7ff559b6e146da3f6d763acae5db5b
Graza <code@graza.io>
2023-05-26 09:27:06 +0100

    fix: code owner review corrected

--- query/branch/default_branch_requires_code_owners_review.sql
+++ query/branch/default_branch_requires_code_owners_review.sql
@@ -2,12 +2,12 @@ select
   -- Required Columns
   url as resource,
   case
-    when (default_branch_ref -> 'branch_protection_rule') is not null and (default_branch_ref -> 'branch_protection_rule' ->> 'requires_approving_reviews')::bool is true  then 'ok'
+    when (default_branch_ref -> 'branch_protection_rule') is not null and (default_branch_ref -> 'branch_protection_rule' ->> 'requires_code_owner_reviews')::bool is true  then 'ok'
     else 'alarm'
   end as status,
     name_with_owner || ' default branch ' || (default_branch_ref ->> 'name') || 
   case 
-    when((default_branch_ref -> 'branch_protection_rule') is not null and (default_branch_ref -> 'branch_protection_rule' ->> 'requires_approving_reviews')::bool is true) then ' enforces code owner''s review.' 
+    when((default_branch_ref -> 'branch_protection_rule') is not null and (default_branch_ref -> 'branch_protection_rule' ->> 'requires_code_owner_reviews')::bool is true) then ' enforces code owner''s review.' 
     else ' does not enforce code owner''s review.' 
   end as reason,
   -- Additional Dimensions
