6f02ff5155be09687767d090f1b50ae9e73ade99
Khushboo <46913995+khushboo9024@users.noreply.github.com>
2025-07-04 19:34:51 +0530

    Error: 'expected 1 arguments, got 0' closes #55 (#56)

--- variables.pp
+++ variables.pp
@@ -6,3 +6,15 @@ locals {
     service  = "GitHub"
   }
 }
+
+variable "trusted_repo_admins" {
+  type        = list(string)
+  default     = [""]
+  description = "A list of github users allowed to delete the repository."
+}
+
+variable "trusted_issue_admins" {
+  type        = list(string)
+  default     = [""]
+  description = "A list of github users allowed to delete the issue."
+}
